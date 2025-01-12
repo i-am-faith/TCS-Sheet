2d8956bcf32b8ca3f96fd06cbb53eaf51cf649ea
i-am-faith <sourin.mukherjee2580@gmail.com>
2025-01-12 14:24:59 +0530

    12 January

--- factorial.java
+++ factorial.java
@@ -3,7 +3,11 @@ import java.util.*;
 class Solution {
 	public static int factorial(int n)
 	{
-		if(n <= 1)
+		if(n < 0)
+		{
+			return 0;
+		}
+		else if(n == 0 || n == 1)
 		{
 			return 1;
 		}
@@ -12,6 +16,10 @@ class Solution {
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		if(factorial(n) == 0)
+		{
+			System.out.println("Error");
+		}
 		System.out.println(factorial(n));
 	}
 }

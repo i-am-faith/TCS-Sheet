a23e1ef395f9ac5194df7c2bb987003c4b7c1cf2
i-am-faith <sourin.mukherjee2580@gmail.com>
2025-06-14 11:54:09 +0530

    added py in search_insert.java

--- search_insert.java
+++ search_insert.java
@@ -24,3 +24,25 @@ public class Solution {
         return ans;
     }
 }
+
+
+/*
+--------------------------------Python-------------------------------------------
+def searchInsert(arr: [int], m: int) -> int:
+    n = len(arr)
+    low = 0
+    high = n-1
+    while low <= high:
+        mid = low + (high-low)//2
+        if arr[mid] == m:
+            return mid
+        elif arr[mid] < m:
+            low = mid+1
+        else:
+            high = mid-1
+
+    return low
+
+
+
+*/
